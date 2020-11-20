cab2b5543926e083264e3967c16b2b45a2f8dce5
Robert Alberto <arobertadrian@gmail.com>
2020-11-20 08:43:16 -0500

    Feature: setup calc with add, subs, and muilt methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,3 +1,15 @@
+def addition(a, b):
+    return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a * b
+
+
 class Calculator:
     result = 0
 
@@ -5,3 +17,15 @@ class Calculator:
         x = 2 + 2
         self.result = x
         pass
+
+    def add(self, a, b):
+        self.result = a + b
+        return addition(a, b)
+
+    def subs(self, a, b):
+        self.result = a - b
+        return subtraction(a, b)
+
+    def mult(self, a, b):
+        self.result = a * b
+        return multiplication(a, b)
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,9 +7,21 @@ class MytestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
-    def test_results_propterty_calculator(self):
+    def test_results_property_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.result, 4)
+
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
+
+    def test_subs_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subs(2, 2), 0)
+
+    def test_mult_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.mult(2, 2), 4)
 
 
 if __name__ == '__main__':
