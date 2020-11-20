9da261ec5b7b5a0054ce5ee345b7458761c50693
Robert Alberto <arobertadrian@gmail.com>
2020-11-20 08:26:18 -0500

    Feature: setup calc with result property

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,6 +7,10 @@ class MytestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_results_propterty_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
